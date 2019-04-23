9c8baa748c790d3a2b370247899cc0838652898b
Liuwei <liuwei03200@gmail.com>
2019-04-23 17:01:57 +0800

    add libai poem

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,7 +17,7 @@
 				<span class="sr-only">Previous</span>
 			</a>
 			<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-				<span class="glyphicon glyphicon-chevron-right" ></span>
+				<span class="glyphicon glyphicon-chevron-right"></span>
 				<span class="sr-only">Next</span>
 			</a>
 			<!-- Indicators -->
@@ -25,9 +25,28 @@
 				<li data-target="#carousel-example-generic" :data-slide-to="n" :class="n==0?'active':''" v-for="v,n in carousel"></li>
 			</ol>
 		</div>
-		<div>
-			<p>dsadsda</p>
+		<h2 class="page-header text-center">李白</h2>
+		<div class="row home-middle">
+			<div class="col-sm-1 col-md-1"></div>
+			<div class="col-sm-10 col-md-10">
+				<div class="row" v-for="v,n in poetry">
+					<div class="col-sm-6 col-md-4" v-for="vv,nn in v">
+						<div class="thumbnail home-bg-gray">
+							<img :src="vv.img" :alt="vv.imgName">
+							<div class="caption text-center">
+								<h3>{{vv.title}}<small>{{vv.dynasty}}·{{vv.author}}</small></h3>
+								<p class="lead " v-if="nnn<=3" v-for="vvv,nnn in vv.info">{{vvv}}</p>
+								<p v-if="vv.info.length>4">
+									<button class="btn btn-primary">展开</button>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-1 col-md-1"></div>
 		</div>
+		
 	</div>
 </template>
 
@@ -36,12 +55,65 @@
 		name: 'Home',
 		data: function() {
 			return {
-				carousel:[
-					{img:'../../static/img/bg_001.jpg',alt:'采桑子'},
-					{img:'../../static/img/bg_002.jpg',alt:'好事近'},
-					{img:'../../static/img/bg_003.jpg',alt:'采桑子'}
+				carousel: [{
+						img: '../../static/img/bg_001.jpg',
+						alt: '采桑子'
+					},
+					{
+						img: '../../static/img/bg_002.jpg',
+						alt: '好事近'
+					},
+					{
+						img: '../../static/img/bg_003.jpg',
+						alt: '采桑子'
+					}
+				],
+				poetry:[
+					[{
+						title:'静夜思',
+						author:'李白',
+						dynasty:'唐',
+						info:['床前明月光，','疑是地上霜。','举头望明月，','低头思故乡。'],
+						img:'../../static/img/libai.jpeg',
+						imgName:'李白-王者荣耀'
+					},{
+						title:'侠客行',
+						author:'李白',
+						dynasty:'唐',
+						info:['赵客缦胡缨，','吴钩霜雪明。','银鞍照白马，','飒沓如流星。','十步杀一人，','千里不留行。','事了拂衣去，','深藏身与名。','闲过信陵饮，','脱剑膝前横。','将炙啖朱亥，','持觞劝侯嬴。','三杯吐然诺，','五岳倒为轻。','眼花耳热后，','意气素霓生。','救赵挥金槌，','邯郸先震惊。','千秋二壮士，','烜赫大梁城。','纵死侠骨香，','不惭世上英。','谁能书阁下，','白首太玄经。'],
+						img:'../../static/img/libai2.jpeg',
+						imgName:'李白-王者荣耀'
+					},{
+						title:'望庐山瀑布',
+						author:'李白',
+						dynasty:'唐',
+						info:['日照香炉生紫烟，','遥看瀑布挂前川。','飞流直下三千尺，','疑是银河落九天。'],
+						img:'../../static/img/libai3.jpeg',
+						imgName:'李白-王者荣耀'
+					}],[{
+						title:'静夜思',
+						author:'李白',
+						dynasty:'唐',
+						info:['床前明月光，疑是地上霜。','举头望明月，低头思故乡。'],
+						img:'../../static/img/libai.jpeg',
+						imgName:'李白-王者荣耀'
+					},{
+						title:'静夜思',
+						author:'李白',
+						dynasty:'唐',
+						info:['床前明月光，疑是地上霜。','举头望明月，低头思故乡。'],
+						img:'../../static/img/libai.jpeg',
+						imgName:'李白-王者荣耀'
+					},{
+						title:'静夜思',
+						author:'李白',
+						dynasty:'唐',
+						info:['床前明月光，疑是地上霜。','举头望明月，低头思故乡。'],
+						img:'../../static/img/libai.jpeg',
+						imgName:'李白-王者荣耀'
+					}]
 				]
-					
+
 			}
 		},
 		methods: {
@@ -57,22 +129,35 @@
 </script>
 
 <style scoped="">
-	.container{
+	.container {
 		margin-top: 50px;
 	}
-	#carousel-example-generic{
-		width:80vw;
-		height:35vw;
-		border-bottom-right-radius:10px;
-		border-bottom-left-radius:10px;
+
+	#carousel-example-generic {
+		width: 80vw;
+		height: 35vw;
+		border-bottom-right-radius: 10px;
+		border-bottom-left-radius: 10px;
 		overflow: hidden;
 	}
-	#carousel-example-generic .carousel-inner{
+
+	#carousel-example-generic .carousel-inner {
 		width: 100%;
 		height: 100%;
-	} 
-	#carousel-example-generic img{
+	}
+
+	#carousel-example-generic img {
 		width: 100%;
 		height: 100%;
+	}
+	.home-middle{
+		margin-top: 15px;
+	}
+	.home-bg-gray{
+		background-color: #E0E0E0;
+	}
+	.home-middle img{
+		width: 100%;
+		height: ;
 	}
 </style>
